36d6960f1e189fab1b222b84f2e1cd8e1027c0d5
DigitalVoid <165980277+Erick-Diego@users.noreply.github.com>
2024-04-24 14:55:15 -0300

    Update dbconfig.js

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -1,11 +1,7 @@
-
-// IMPORTA O MONGOOESE PARA O PROJETO
 const mongoose = require('mongoose');
 
-// COLOCA DENTRO DA CONSTANCIA DECONFIG A URL DE CONEXÃO
 const Dbconfig = 'mongodb+srv://<usuario>:<senha>@cluster1.4l1sdky.mongodb.net/Gallery';
 
-// MONGOOSE CONECTAR NA VARIAVEL DBCONFIG
 mongoose.connect(Dbconfig)
     .then(() => {
         console.log("Banco Conectado com Sucesso!");
