764db344f630fbba95d603719c36256726cc34b5
DigitalVoid <165980277+Erick-Diego@users.noreply.github.com>
2024-04-24 14:59:19 -0300

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,24 +1,20 @@
 const User = require('../models/userModel');
-const fs = require('fs/promises'); // Módulo FileSystem para trabalhar com arquivos
+const fs = require('fs/promises');
 const path = require('path');
 
 const updateBiografia = async (req, res) => {
     try {
-        // Obtém o ID do usuário autenticado a partir do token
         const userId = req.user.id;
-
-        // Verifica se o usuário existe
+        
         const user = await User.findById(userId);
         if (!user) {
             return res.status(404).json({ message: 'Usuário não encontrado' });
         }
 
-        // Verifica se a biografia foi enviada na requisição
         if (!req.body.biografia) {
             return res.status(400).json({ message: 'Biografia não fornecida' });
         }
 
-        // Atualiza a biografia do usuário
         user.biografia = req.body.biografia;
         await user.save();
 
@@ -30,28 +26,23 @@ const updateBiografia = async (req, res) => {
 
 const updateFotoPerfil = async (req, res) => {
     try {
-        // Obtém o ID do usuário autenticado a partir do token
         const userId = req.user.id;
 
-        // Verifica se o usuário existe
         const user = await User.findById(userId);
         if (!user) {
             return res.status(404).json({ message: 'Usuário não encontrado' });
         }
 
-        // Remove a foto antiga se existir
-        if (user.fotoPerfil && user.fotoPerfil !== 'C:/Users/User/OneDrive/Área de Trabalho/MyGallery/backend/src/upload/foto-user/user_icon-icons.com_66546.png') {
+        if (user.fotoPerfil && user.fotoPerfil !== '<Caminho para a foto base de Usuario>') {
             const fotoAntigaPath = user.fotoPerfil;
-            await fs.unlink(fotoAntigaPath); // Remove a foto antiga
+            await fs.unlink(fotoAntigaPath);
         }
 
-        // Verifica se a foto foi enviada na requisição
         if (!req.file) {
             return res.status(400).json({ message: 'Foto de perfil não fornecida' });
         }
 
-        // Atualiza a foto de perfil do usuário
-        user.fotoPerfil = req.file.path; // Caminho da nova foto
+        user.fotoPerfil = req.file.path;
         await user.save();
 
         res.status(200).json({ message: 'Foto de perfil atualizada com sucesso' });
@@ -63,21 +54,18 @@ const updateFotoPerfil = async (req, res) => {
 
 const addDefaultProfilePhoto = async (userId) => {
     try {
-        // Verifica se o usuário existe
         const user = await User.findById(userId);
         if (!user) {
             console.error('Usuário não encontrado');
             return;
         }
 
-        // Verifica se o usuário já possui uma foto de perfil
         if (user.fotoPerfil) {
             console.log('Usuário já possui uma foto de perfil');
             return;
         }
 
-        // Adiciona uma foto de perfil padrão
-        const defaultPhotoPath = 'C:/Users/User/OneDrive/Área de Trabalho/MyGallery/backend/src/upload/foto-user/user_icon-icons.com_66546.png'; // Coloque o caminho para a foto padrão aqui
+        const defaultPhotoPath = '<Caminho para a foto base de Usuario>'; 
         user.fotoPerfil = defaultPhotoPath;
         await user.save();
 
@@ -89,26 +77,21 @@ const addDefaultProfilePhoto = async (userId) => {
 
 const removeFotoPerfil = async (req, res) => {
     try {
-        // Obtém o ID do usuário autenticado a partir do token
         const userId = req.user.id;
 
-        // Verifica se o usuário existe
         const user = await User.findById(userId);
         if (!user) {
             return res.status(404).json({ message: 'Usuário não encontrado' });
         }
 
-        // Verifica se o usuário já tem uma foto de perfil
-        if (!user.fotoPerfil || user.fotoPerfil === 'C:/Users/User/OneDrive/Área de Trabalho/MyGallery/backend/src/upload/foto-user/user_icon-icons.com_66546.png') {
+        if (!user.fotoPerfil || user.fotoPerfil === '<Caminho para a foto base de Usuario>') {
             return res.status(400).json({ message: 'Usuário não possui uma foto de perfil para remover' });
         }
 
-        // Remove a foto antiga
         const fotoAntigaPath = user.fotoPerfil;
-        await fs.unlink(fotoAntigaPath); // Remove a foto antiga
+        await fs.unlink(fotoAntigaPath); 
 
-        // Atribui a foto base como foto de perfil
-        user.fotoPerfil = 'C:/Users/User/OneDrive/Área de Trabalho/MyGallery/backend/src/upload/foto-user/user_icon-icons.com_66546.png'; // Caminho da foto base
+        user.fotoPerfil = '<Caminho para a foto base de Usuario>';
         await user.save();
 
         res.status(200).json({ message: 'Foto de perfil removida com sucesso' });
@@ -116,7 +99,5 @@ const removeFotoPerfil = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
-
-
 
 module.exports = { updateBiografia, updateFotoPerfil, addDefaultProfilePhoto, removeFotoPerfil };
